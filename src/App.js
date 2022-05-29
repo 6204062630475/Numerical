ea08f3c948eeb6f28a897635434539e729c88b7c
6204062630475 <chayakorn1106@gmail.com>
2022-05-29 15:00:21 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import { Route, Routes } from 'react-router-dom';
 import AppBar from './Component/AppBar';
 import Bisection from './Component/Bisection';
 import FalsePosition from "./Component/FalsePosition";
-import  Newton  from "./Component/Newton";
+import Newton from "./Component/Newton";
 import Equationlist from "./Component/Equationlist"
-
+import { BrowserRouter } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <AppBar />
-      <Routes>
-      <Route path="/" element={<Bisection />} />
-        <Route path="/Bisection" element={<Bisection />} />
-        <Route path="/FalsePosition" element={<FalsePosition />}/>
-        <Route path="/Newton" element={<Newton/>} />
-        <Route path="/Equations" element={<Equationlist />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Bisection />} />
+          <Route path="/Bisection" element={<Bisection />} />
+          <Route path="/FalsePosition" element={<FalsePosition />} />
+          <Route path="/Newton" element={<Newton />} />
+          <Route path="/Equations" element={<Equationlist />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
